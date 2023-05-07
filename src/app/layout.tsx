1649a953e48dc87e7721d216825b0a767e954f05
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2023-05-07 02:04:41 -0300

    Adicionado Chadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Signika } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const signika = Signika({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={signika.className}>{children}</body>
     </html>
   );
 }
