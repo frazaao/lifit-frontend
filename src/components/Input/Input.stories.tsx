f8288a156801cfbd3a65f1b1e872d6b6082161cf
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2023-05-11 21:27:47 -0300

    Update Input.stories.tsx

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,15 +1,15 @@
-import { Meta, StoryObj } from "@storybook/react";
-import Input from ".";
+//import { Meta, StoryObj } from "@storybook/react";
+//import Input from ".";
 
-const meta: Meta<typeof Input> = {
-  title: "Input",
-  component: Input,
-};
+//const meta: Meta<typeof Input> = {
+//  title: "Input",
+//  component: Input,
+//};
 
-export default meta;
+//export default meta;
 
-type Story = StoryObj<typeof Input>;
+//type Story = StoryObj<typeof Input>;
 
-export const Default: Story = {
-  args: {},
-};
+//export const Default: Story = {
+//  args: {},
+//};
