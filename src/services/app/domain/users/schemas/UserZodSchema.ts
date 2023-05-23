7214fb8e12a4fd4e44a93b3f5ad04694cdc3f685
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2023-05-22 21:35:59 -0300

    Ajustado schema do zod para ficar de acordo com tipagem

--- src/services/app/domain/users/schemas/UserZodSchema.ts
+++ src/services/app/domain/users/schemas/UserZodSchema.ts
@@ -2,7 +2,15 @@ import { z } from "zod";
 import UserDomain from "../types/UserDomain";
 
 const userZodSchema: z.ZodType<UserDomain> = z.object({
-    //
+    name: z.string().nonempty(
+        "O campo nome é obrigatório"
+    ),
+    role: z.enum(["ADMIN", "PATIENT", "NUTRITIONIST"]),
+    email: z.string().nonempty(
+        "O campo email é obrigatório"
+    ).email(
+        "O campo email é obrigatório"
+    )
 });
 
 export default UserZodSchema;
