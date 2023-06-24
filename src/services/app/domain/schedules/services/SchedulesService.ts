2ef7a10d456e8aa27d0bbe747e0d5a51323d1b37
Matheus Frazão <matheus_paula2012@hotmail.com>
2023-06-24 12:44:55 -0300

    Quase finalizado funcionalidade de agendamento

--- src/services/app/domain/schedules/services/SchedulesService.ts
+++ src/services/app/domain/schedules/services/SchedulesService.ts
@@ -1,40 +1,32 @@
-import Service from "@/services/app/contracts/Service";
 import ScheduleDomain from "../types/ScheduleDomain";
 import SchedulePersistence from "../types/SchedulePersistence";
 import ScheduleMapper from "../mappers/ScheduleMapper";
 
-class SchedulesService
-    implements
-        Service<
-            ScheduleDomain,
-            null,
-            null
-        >
-{
+class SchedulesService {
     prefix = "/schedule/";
 
-    async create(schedule: ScheduleDomain): Promise<ScheduleDomain> {
-        //
-    }
+    // async create(schedule: ScheduleDomain): Promise<ScheduleDomain> {
+    //     //
+    // }
 
-    async find(id: string | number): Promise<ScheduleDomain> {
-        //
-    }
+    // async find(id: string | number): Promise<ScheduleDomain> {
+    //     //
+    // }
 
-    async list(): Promise<ScheduleDomain[]> {
-        //
-    }
+    // async list(): Promise<ScheduleDomain[]> {
+    //     //
+    // }
 
-    async update(
-        id: string | number,
-        schedule: ScheduleDomain
-    ): Promise<ScheduleDomain> {
-        //
-    }
+    // async update(
+    //     id: string | number,
+    //     schedule: ScheduleDomain
+    // ): Promise<ScheduleDomain> {
+    //     //
+    // }
 
-    async delete(id: string | number): Promise<void> {
-        //
-    }
+    // async delete(id: string | number): Promise<void> {
+    //     //
+    // }
 }
 
 export default new SchedulesService();
